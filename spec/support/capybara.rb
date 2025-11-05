2ff5b7d56fc3f674ad7f8e9d35f8510b97a7c524
Elison de Campos <elison.campos@gmail.com>
2025-11-05 09:19:10 -0300

    :recycle: Improve capybara config and ci workflow

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -5,7 +5,10 @@ require 'selenium-webdriver'
 
 # Configure Capybara
 Capybara.configure do |config|
-  config.default_max_wait_time = 5
+  # Increase timeout for slow CI environments
+  config.default_max_wait_time = ENV.fetch('CAPYBARA_TIMEOUT', 10).to_i
+  config.default_driver = :rack_test
+  config.javascript_driver = :headless_chrome
   config.server = :puma, { Silent: true }
   config.server_host = '127.0.0.1'
   config.server_port = 3000
@@ -19,11 +22,33 @@ Capybara.register_driver :headless_chrome do |app|
   options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--disable-gpu')
   options.add_argument('--window-size=1400,1400')
+  options.add_argument('--disable-software-rasterizer')
+  options.add_argument('--disable-extensions')
+  options.add_argument('--disable-background-timer-throttling')
+  options.add_argument('--disable-backgrounding-occluded-windows')
+  options.add_argument('--disable-renderer-backgrounding')
+  options.add_argument('--disable-features=TranslateUI')
+  options.add_argument('--disable-ipc-flooding-protection')
+
+  # Set page load timeout
+  options.page_load_strategy = :normal
 
   # Enable browser console logs
   options.logging_prefs = { browser: 'ALL' }
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  driver = Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options: options,
+    timeout: ENV.fetch('CAPYBARA_TIMEOUT', 10).to_i
+  )
+
+  # Set explicit timeouts
+  driver.browser.manage.timeouts.implicit_wait = ENV.fetch('CAPYBARA_TIMEOUT', 10).to_i
+  driver.browser.manage.timeouts.page_load = 30
+  driver.browser.manage.timeouts.script_timeout = 30
+
+  driver
 end
 
 # Use headless Chrome for JavaScript tests
@@ -33,5 +58,14 @@ RSpec.configure do |config|
   # Configure Capybara for system specs
   config.before(:each, type: :system) do
     driven_by :headless_chrome
+  end
+
+  # Force cleanup of all Chrome processes after suite
+  config.after(:suite) do
+    # Kill any remaining Chrome processes (in CI only)
+    if ENV['CI']
+      system('pkill -9 chrome 2>/dev/null', out: File::NULL, err: File::NULL)
+      system('pkill -9 chromedriver 2>/dev/null', out: File::NULL, err: File::NULL)
+    end
   end
 end
